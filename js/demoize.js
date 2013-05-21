5af7dba3021dbc3a5eb608b7b98c5ec1905d9b06
Kurt Ruppel <me@kurtruppel.com>
2013-05-21 08:24:52 -0700

    Style changes.

--- js/demoize.js
+++ js/demoize.js
@@ -1,3 +1,11 @@
 (function (window, undefined) {
-  $('.search').instantSearch();
+  $('.search').instantSearch({
+    source: function (req, res) {
+      return res([
+        'Abba',
+        'Dabba',
+        'Doo'
+      ]);
+    }
+  }).focus();
 }(window));
